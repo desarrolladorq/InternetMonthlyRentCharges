4d25cab67a83da517bcc6a54de11cae85f343abf
air <air@MacBook-Air-de-air.local>
2016-10-03 16:45:17 -0500

    feat : inflate the view for MainActivityFragment and also inflate the listview on it

--- app/src/main/java/test/example/internetmonthlyrentcharges/MainActivityFragment.java
+++ app/src/main/java/test/example/internetmonthlyrentcharges/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +18,15 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        // inflate the view for MainActivityFragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Get a reference to a ListView, and attach this adapter to it
+        ListView listView = (ListView) view.findViewById(
+                R.id.listview);
+
+        return view;
     }
 }

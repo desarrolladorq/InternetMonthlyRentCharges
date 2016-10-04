42937cf79ae03c92f81a15b7517f97598a0e1746
air <air@MacBook-Air-de-air.local>
2016-10-04 14:41:29 -0500

    style : layouts for the dialog window

---
+++ app/src/main/java/test/example/internetmonthlyrentcharges/models/UserNameModel.java
@@ -0,0 +1,20 @@
+package test.example.internetmonthlyrentcharges.models;
+
+/**
+ * Created by air on 19/09/16.
+ */
+
+public class UserNameModel {
+    public UserNameModel() {
+    }
+
+    String username;
+
+    public UserNameModel(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}

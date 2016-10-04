5b3121a39183059e3f848274d6b129468682ea79
air <air@MacBook-Air-de-air.local>
2016-10-03 20:10:45 -0500

    feat : Add android INTERNET permission, create a reference to firebase, and also set up the name of Application to be InternetMonthlyCharges2Application in AndroidManifest xml file.

--- app/src/main/java/test/example/internetmonthlyrentcharges/MainActivityFragment.java
+++ app/src/main/java/test/example/internetmonthlyrentcharges/MainActivityFragment.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.firebase.client.Firebase;
+
+import test.example.internetmonthlyrentcharges.constants.Constants;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -19,13 +23,17 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         // inflate the view for MainActivityFragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         //Get a reference to a ListView, and attach this adapter to it
         ListView listView = (ListView) view.findViewById(
                 R.id.listview);
+
+        // Get a reference from firebase to a ref call 'usernames' to populate the listview
+        Firebase ref = new Firebase(Constants.MONTLYINTERNETUSERSREF).child(Constants.ACTIVEUSERS);
+
+
 
         return view;
     }
---
+++ app/src/main/java/test/example/internetmonthlyrentcharges/constants/Constants.java
@@ -0,0 +1,18 @@
+package test.example.internetmonthlyrentcharges.constants;
+
+/**
+ * Created by air on 10/09/16.
+ */
+
+public class Constants {
+
+    // Constant to store the link reference to firebase database
+    public final static String MONTLYINTERNETUSERSREF = "https://montlyinternetusers.firebaseio.com/";
+    // Constant to store only the usernames in firebase
+    public final static String ACTIVEUSERS = "activeUsers";
+    // Constant to store the branch structure that store the attributes of the user in firebase
+    public final static String USERS_ATRIBUTES = "usersAtributes";
+    public final static String SUSPENDEDUSERS = "suspendedUsers";
+    // Constant to store the branch structure "usersMontlyCharges" in firebase databbase
+    public final static String USERSMONTHLYCHARGES = "usersMontlyCharges";
+}

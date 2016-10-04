04b2197a37f559d54238cbdb85cfc995e64264d7
air <air@MacBook-Air-de-air.local>
2016-10-04 18:53:16 -0500

    feat : set a firebaseAdapter with data from a ref and populate a listView

--- app/src/main/java/test/example/internetmonthlyrentcharges/MainActivityFragment.java
+++ app/src/main/java/test/example/internetmonthlyrentcharges/MainActivityFragment.java
@@ -6,15 +6,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseListAdapter;
 
 import test.example.internetmonthlyrentcharges.constants.Constants;
+import test.example.internetmonthlyrentcharges.models.UserNameModel;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    // variable to store a FirebaseListAdapter
+    FirebaseListAdapter<UserNameModel> firebaseListAdapter;
+
+    // variable to store a ListView
+    ListView listView;
 
     public MainActivityFragment() {
     }
@@ -27,13 +35,33 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         //Get a reference to a ListView, and attach this adapter to it
-        ListView listView = (ListView) view.findViewById(
+        listView = (ListView) view.findViewById(
                 R.id.listview);
 
-        // Get a reference from firebase to a ref call 'usernames' to populate the listview
-        Firebase ref = new Firebase(Constants.MONTLYINTERNETUSERSREF).child(Constants.ACTIVEUSERS);
-
+        // set the adapter with firebase data and populate the list view
+        setAdapterAndPopulateListView();
 
         return view;
+    }
+
+    private void setAdapterAndPopulateListView(){
+        // Get a reference from firebase to a ref call 'usernames' to populate the listview
+        Firebase ref = new Firebase(Constants.MONTLYINTERNETUSERSREF).child(Constants.ACTIVEUSERS);
+        // set the adapter with the firebase ref and also give a model to land the data on it
+        firebaseListAdapter = new FirebaseListAdapter<UserNameModel>(
+                getActivity(),
+                UserNameModel.class,
+                R.layout.list_item_user_name,
+                ref
+        ) {
+            @Override
+            protected void populateView(View view, UserNameModel userNameModel) {
+                // we get the name of the user from the model
+                String username = userNameModel.getUsername();
+                TextView textView  = (TextView)view.findViewById(R.id.list_item_name);
+                textView.setText(username);
+            }
+        };
+        listView.setAdapter(firebaseListAdapter);
     }
 }
